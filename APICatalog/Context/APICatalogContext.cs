029c9407b9708c23a550733937d389b76ed52aff
narba <narba@DESKTOP-KRRRREC>
2022-08-23 00:39:57 -0300

    Add project files.

---
+++ APICatalog/Context/APICatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using APICatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalog.Context;
+
+public class APICatalogContext : DbContext
+{
+    //Construtor \/
+    public APICatalogContext(DbContextOptions<APICatalogContext> options) : base(options)
+    { }
+
+    // Mapeamento das minhas entidades \/
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet <Produto>? Produtos { get; set; }
+
+}
---
+++ APICatalog/Models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalog.Models;
+
+public class Categoria
+{
+    #region Propriedades
+    public int CategoriaId  { get; set; } // Chave primária
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; } // Cada categoria pode ter uma coleção de produtos
+    #endregion
+
+    #region Construtor
+    public Categoria()
+    {
+        Produtos = new List<Produto>();
+    }
+    #endregion
+}
+
